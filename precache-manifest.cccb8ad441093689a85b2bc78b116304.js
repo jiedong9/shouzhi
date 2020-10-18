35f810b28ea40483743f809ec77adbfdf0686466
jiedong <785934566@qq.com>
2020-10-18 14:21:12 +0800

    deploy

---
+++ precache-manifest.cccb8ad441093689a85b2bc78b116304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad826f6741c1fd2a617",
+    "url": "/shouzhi/css/app.87a705fe.css"
+  },
+  {
+    "revision": "813744598531646b10fe7bea12c21cee",
+    "url": "/shouzhi/index.html"
+  },
+  {
+    "revision": "bad826f6741c1fd2a617",
+    "url": "/shouzhi/js/app.661c18ce.js"
+  },
+  {
+    "revision": "b817c764696344722bf0",
+    "url": "/shouzhi/js/chunk-vendors.de5998fa.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/shouzhi/manifest.json"
+  },
+  {
+    "revision": "54474bba3b0a2baf4f7565d7cd61390d",
+    "url": "/shouzhi/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/shouzhi/robots.txt"
+  }
+]);
